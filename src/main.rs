21e6c941604e1c99be6df26af82d1e5a5c4311b8
Rok Hudournik <167877897+lejgabottih@users.noreply.github.com>
2025-05-13 17:02:07 +0200

    Rok (#15)
    * made boost and mod_distance function
    
    * majnsi popravek na za mod (spacesize)...
    
    * dodanih par testov
    
    * spremenjeni testi na 1e-3

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn main() {
 
     loop {
         if is_key_pressed(KeyCode::Escape) || is_key_pressed(KeyCode::Space) {
-            paused = !paused;
+            paused = !paused; //treba spremenit ker lahka tut ko je gameover klikas (včasih se ne pokaze kvadrat cez cel ekrat ko je konc)
         }
 
         let dt = get_frame_time();
@@ -44,7 +44,14 @@ async fn main() {
             view.rotate(dt);
 
             player.set_direction(view.forward());
-            player.move_forward(dt);
+
+            player.check_boost_and_move(dt);
+
+            if player.check_boost_time(&mut food_factory, snake_start_len) {
+                paused = true;
+                game_over = true;
+            }
+
 
             if player.check_tail_collision() {
                 paused = true;
@@ -96,6 +103,7 @@ async fn main() {
                 player.set_direction(vec3(1., 0., 0.));
                 player.reset();
                 view.reset();
+                food_factory.reset();
                 game_over = false;
                 for _ in 0..snake_start_len {
                     player.add_segment();
